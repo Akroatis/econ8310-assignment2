0207dcab40b836aa6d58079186d0067acd648562
Akroatis <49747834+Akroatis@users.noreply.github.com>
2025-10-08 20:22:00 -0500

    Attempt 5
    Modified test data so that the Xt that's being tested on matches the 1000 results it needs.

--- assignment2.py
+++ assignment2.py
@@ -6,7 +6,7 @@ from xgboost import XGBClassifier
 
 data = pd.read_csv("https://github.com/dustywhite7/Econ8310/raw/master/AssignmentData/assignment3.csv")
 testData = pd.read_csv("https://github.com/dustywhite7/Econ8310/raw/master/AssignmentData/assignment3test.csv")
-testData = testData.head(1000)
+
 
  # Upper case before split, lower case after
 Y = data['meal']
@@ -16,10 +16,14 @@ X = data.drop(['meal', 'id', 'DateTime'], axis=1)
 #x, xt, y, yt = train_test_split(X, Y, test_size=0.2, random_state=42)
 
 #Since we actually ahve test data, no split, instead train on everything and test on given data
+#Modifications to the test data, too, because it's utterly scuffed how this has to fit.
 Yt = data['meal']
+Yt = Yt.head(1000)
 Xt = data.drop(['meal', 'id', 'DateTime'], axis=1)
+Xt = Xt.head(1000)
 
-model = XGBClassifier(n_estimators=1000, max_depth=15, learning_rate=1, objective='binary:logistic')
+model = XGBClassifier(n_estimators=1000, max_depth=13, learning_rate=1, objective='binary:logistic')
+
 
 modelFit = model.fit(X, Y)
 
